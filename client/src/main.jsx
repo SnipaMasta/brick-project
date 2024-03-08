8d1b03ae4b96f7009a14e30362aedd56b93d5ed2
Chris Rice <chrisrice2442@yahoo.com>
2024-03-07 19:02:26 -0500

    adding components and pages for react

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,33 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/Login',
+        element: <Login />,
+      },
+      // {
+      //   path: '/Blog',
+      //   element: <Blog />,
+      // },
+      // {
+      //   path: '/Contact',
+      //   element: <Contact />,
+      // },
+    ],
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
